2c5d7cc949ed34825d974504dce698b9df6e976c
unknown <Administrador@LIM03TELEDIGUZM.emcali.com.co>
2025-11-19 15:43:48 -0500

    se agrega modelo de perceptron, sus graficas y metricas. Se crean metricas Random Forest, regresion Lineal y SVR en excel

---
+++ 17NOVIEMBRE2025-Entregables/11.Perceptron_por_zona.py
@@ -0,0 +1,199 @@
+# Tratamiento de datos
+# ==============================================================================
+import numpy as np
+import pandas as pd
+from skforecast.datasets import fetch_dataset
+
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+from skforecast.plot import set_dark_theme
+
+# Modelado y Forecasting
+# ==============================================================================
+import sklearn
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+import skforecast
+from skforecast.recursive import ForecasterRecursive
+from skforecast.direct import ForecasterDirect
+from skforecast.model_selection import TimeSeriesFold, grid_search_forecaster, backtesting_forecaster
+from skforecast.preprocessing import RollingFeatures
+from skforecast.utils import save_forecaster, load_forecaster
+from skforecast.metrics import calculate_coverage
+from skforecast.plot import plot_prediction_intervals
+from sklearn.preprocessing import RobustScaler
+from sklearn.svm import SVR
+import shap
+
+import math
+import glob
+import os
+from pathlib import Path
+
+GRAF_DIR = "Perceptron_Graficas"
+os.makedirs(GRAF_DIR, exist_ok=True)
+
+ORIGEN = "csv-zonas-wifi-de-pamartin/"
+#DESTINO_METRICAS = "Random_Forest_Metricas"
+DESTINO_METRICAS = Path("Perceptron_Metricas")
+os.makedirs(DESTINO_METRICAS, exist_ok=True)
+
+#carpeta = os.path.join(os.path.dirname(__file__), "csv-zonas-wifi-1AP-todas-las-columnas")
+#print(carpeta)
+
+archivos = glob.glob(os.path.join(ORIGEN, "*.csv"))
+
+#df_errors = pd.DataFrame(columns=["Zona", "y_true", "y_pred", "MAPE", "error_abs", "error_relativo"])
+df_errors = pd.DataFrame(columns=["Zona", "MAPE", "MAE", "RMSE"])
+mape_percent = 0
+
+for archivo in archivos:
+    nombre_zona = os.path.basename(archivo)
+    print(f"\nProcesando: {nombre_zona}")
+    
+    #df = pd.read_csv(os.path.join(carpeta,"001_ZW Parque Ingenio-test2.csv"))
+    df = pd.read_csv(archivo)
+    
+    # Preparación del dato
+    # ==============================================================================
+    df['FECHA_CONEXION'] = pd.to_datetime(df['FECHA_CONEXION'], format='%Y-%m-%d')
+    df = df.set_index('FECHA_CONEXION')
+    df = df.asfreq('D') # Fechas con frecuencia diario. En caso de que falte algún día se crea y todas las demás variables se ponen NaN
+    
+    df = df.sort_index() # En caso de que esten las fechas desorganizadas se organizan de forma ascendente
+    
+    # Hacer método para detectar si hay datos faltantes y no proseguir con el modelado
+    df.isnull().any(axis=1).mean()
+    
+    rows_with_na = df[df.isna().any(axis=1)]
+    print("\nRows with any NA values:")
+    print(rows_with_na)
+    
+    rows, columns = df.shape
+    print(rows)
+    
+    df['DIA_SEMANA'] = df['DIA_SEMANA'].astype('Int64')
+    df['LABORAL'] = df['LABORAL'].astype('Int64')
+    df['FIN_DE_SEMANA'] = df['FIN_DE_SEMANA'].astype('Int64')
+    df['FESTIVO'] = df['FESTIVO'].astype('Int64')
+    df['PORCENTAJE_USO'] = df['PORCENTAJE_USO'].astype('Float64')
+    df['NUMERO_CONEXIONES'] = df['NUMERO_CONEXIONES'].astype('Float64')
+    df['USAGE_KB'] = df['USAGE_KB'].astype('Float64')
+    
+    # Separamos el dataset en 70% de train y 30% de test
+    steps = rows*0.3 # 20% en test
+    steps = math.floor(steps)
+    print(f"Dataset separado con {steps} filas en test y {rows-steps} filas en la parte train")
+    
+    # Separación datos train-test
+    # ==============================================================================
+    df_train = df[:-steps]
+    df_test  = df[-steps:]
+    print(f"Fechas train : {df_train.index.min()} --- {df_train.index.max()}  (n={len(df_train)})")
+    print(f"Fechas test  : {df_test.index.min()} --- {df_test.index.max()}  (n={len(df_test)})")
+    
+    # Variables de entrada para el modelo
+    exog_variables = ['DIA_SEMANA', 'LABORAL', 'FIN_DE_SEMANA', 'FESTIVO', 'PORCENTAJE_USO', 'NUMERO_CONEXIONES']
+    # Define las variables exogenas (usa las versiones escaladas para variables continuas)
+    exog_variables_scaled = ['DIA_SEMANA', 'LABORAL', 'FIN_DE_SEMANA', 'FESTIVO', 'PORCENTAJE_USO_scaled', 'NUMERO_CONEXIONES_scaled']
+
+    # Inicializar escaladores para cada variable
+    scaler_usage = RobustScaler()
+    scaler_conexiones = RobustScaler()
+    scaler_porcentaje = RobustScaler()
+
+    # Se escala la data de training
+    df_train['USAGE_KB_scaled'] = scaler_usage.fit_transform(df_train[['USAGE_KB']])
+    df_train['NUMERO_CONEXIONES_scaled'] = scaler_conexiones.fit_transform(df_train[['NUMERO_CONEXIONES']])
+    df_train['PORCENTAJE_USO_scaled'] = scaler_porcentaje.fit_transform(df_train[['PORCENTAJE_USO']])
+
+    # Se escala test aplicando los escaladores de train (Se usa "transform", no "fit_transform")
+    df_test['USAGE_KB_scaled'] = scaler_usage.transform(df_test[['USAGE_KB']])
+    df_test['NUMERO_CONEXIONES_scaled'] = scaler_conexiones.transform(df_test[['NUMERO_CONEXIONES']])
+    df_test['PORCENTAJE_USO_scaled'] = scaler_porcentaje.transform(df_test[['PORCENTAJE_USO']])
+
+    # Perceptron MLP
+    forecaster = ForecasterRecursive(
+        regressor=MLPRegressor(
+            hidden_layer_sizes=(100, 50),  # Two hidden layers
+            activation='relu',
+            solver='adam',
+            max_iter=1000,
+            random_state=123
+        ),
+        lags=8
+    )
+
+    # Ajuste con variables exogenas
+    forecaster.fit(
+        y=df_train['USAGE_KB_scaled'],
+        exog=df_train[exog_variables_scaled]  # Add exogenous variables here
+    )
+
+    # Make predictions (returns scaled predictions)
+    predictions_scaled = forecaster.predict(
+        steps=steps,
+        exog=df_test[exog_variables_scaled]  # Future scaled exogenous variables
+    )
+
+    # Descaling the predictions
+    predictions_final = pd.Series(
+        scaler_usage.inverse_transform(predictions_scaled.values.reshape(-1, 1)).flatten(),
+        index=predictions_scaled.index
+    )
+
+
+    # Mean Absolute Percentage Error
+    mape = mean_absolute_percentage_error(
+        y_true=df_test['USAGE_KB'],
+        y_pred=predictions_final
+    )
+    mape_percent = mape*100
+    print(f"MAPE: {mape:.4f} ({mape*100:.2f}%)")
+
+    # Mean Absolute Error
+    mae = mean_absolute_error(df_test['USAGE_KB'], predictions_final)
+    print(f"MAE: {mae:.2f}")
+
+    # Root Mean Squared Error
+    mse = mean_squared_error(df_test['USAGE_KB'], predictions_final)
+    rmse = np.sqrt(mse)
+    print(f"RMSE: {rmse:.2f}")
+
+    new_row = pd.DataFrame([{"Zona": nombre_zona, "MAPE": mape_percent, "MAE": mae, "RMSE": rmse}])
+    df_errors = pd.concat([df_errors, new_row], ignore_index=True)
+
+    #print(predictions,df_test['USAGE.KB'])
+    usage_kb_compared = pd.DataFrame({
+        'USAGE_KB_predicho': predictions_final,
+        'USAGE_KB_real': df_test['USAGE_KB']
+    })
+    difference = predictions_final - df_test['USAGE_KB']
+    usage_kb_compared['error_absoluto'] = difference.abs()
+    usage_kb_compared['error_relativo'] = usage_kb_compared['error_absoluto'] / usage_kb_compared['USAGE_KB_real']
+    print(usage_kb_compared)
+
+    usage_kb_compared.to_csv(DESTINO_METRICAS / f"metricas_{nombre_zona}", index=False, encoding='utf-8')
+    
+    # Graficar serie temporal
+    #print(plt.style.available)
+    plt.style.use('seaborn-v0_8-dark')
+    plt.figure(figsize=(10, 5))
+    plt.plot(df_train['USAGE_KB'], label="Train", linewidth=2)
+    plt.plot(df_test['USAGE_KB'], label="Test", linewidth=2)
+    plt.plot(predictions_final, label="Predicho", linewidth=2)
+    plt.title(f"Random Forest - {nombre_zona}")
+    plt.xlabel("Índice temporal")
+    plt.ylabel("Tráfico (kB)")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(os.path.join(GRAF_DIR, f"{nombre_zona}_serie.png"), dpi=300)
+    plt.close()
+
+df_errors.to_csv(DESTINO_METRICAS / "metricas.csv", index=False, encoding='utf-8')
