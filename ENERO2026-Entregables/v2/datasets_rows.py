c1dc7e612a5ca3a91ed3e3f8ee3a84a5f1bd731c
unknown <Administrador@LIM03TELEDIGUZM.emcali.com.co>
2026-01-08 13:38:20 -0500

    se sube script que muestra el tamaño de todos los datasets y se sube el CSV con dicha informacion

---
+++ ENERO2026-Entregables/v2/datasets_rows.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import glob
+import os
+from pathlib import Path
+
+# Archivo que muestra el tamaño en filas de cada dataset y guarda la información en un CSV "datasets_rows"
+
+ORIGEN = "csv-zonas-wifi-separados-man/"
+
+DATASETS = Path("datasets_rows")
+os.makedirs(DATASETS, exist_ok=True)
+
+archivos = glob.glob(os.path.join(ORIGEN, "*.csv"))
+
+df_lengths = pd.DataFrame(columns=["Zona", "Rows"])
+
+for archivo in archivos:
+    
+    nombre_zona = os.path.basename(archivo)
+
+    df = pd.read_csv(archivo)
+    print(f"{nombre_zona}:")
+    length_rows = len(df)
+    print(length_rows)
+
+    new_row = pd.DataFrame([{"Zona": nombre_zona, "Rows": length_rows}])
+    df_lengths = pd.concat([df_lengths, new_row], ignore_index=True)
+
+df_lengths.to_csv(DATASETS / f"datasets_rows.csv", index=False, encoding='utf-8')
