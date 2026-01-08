5cad12fc0245fbf686e646e79e261a33b52253d0
unknown <Administrador@LIM03TELEDIGUZM.emcali.com.co>
2026-01-08 13:42:34 -0500

    se actualiza el script y el CSV

--- ENERO2026-Entregables/v2/datasets_rows.py
+++ ENERO2026-Entregables/v2/datasets_rows.py
@@ -17,13 +17,14 @@ df_lengths = pd.DataFrame(columns=["Zona", "Rows"])
 for archivo in archivos:
     
     nombre_zona = os.path.basename(archivo)
+    nombre_zona_recortado = nombre_zona[:-4]
 
     df = pd.read_csv(archivo)
-    print(f"{nombre_zona}:")
+    print(f"{nombre_zona_recortado}:")
     length_rows = len(df)
     print(length_rows)
 
-    new_row = pd.DataFrame([{"Zona": nombre_zona, "Rows": length_rows}])
+    new_row = pd.DataFrame([{"Zona": nombre_zona_recortado, "Rows": length_rows}])
     df_lengths = pd.concat([df_lengths, new_row], ignore_index=True)
 
 df_lengths.to_csv(DATASETS / f"datasets_rows.csv", index=False, encoding='utf-8')
